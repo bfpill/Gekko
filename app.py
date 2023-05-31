9a6f800342138ce1d3b16fe46b8616a7607d53b4
bfpill <max@v3rv.com>
2023-05-31 20:32:51 +1000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify("hello")
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    new_user = request.get_json()
+    users.append(new_user)
+    return jsonify(new_user), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
