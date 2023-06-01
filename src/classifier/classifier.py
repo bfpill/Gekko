d5b97e6fcf127fcbdfe95bb221b6a896992657fa
bfpill <max@v3rv.com>
2023-06-01 21:25:30 +1000

    added the start of a test memory_writer and the services

--- src/classifier/classifier.py
+++ src/classifier/classifier.py
@@ -3,100 +3,93 @@ from langchain.prompts import StringPromptTemplate
 from langchain import OpenAI, SerpAPIWrapper, LLMChain
 from typing import List, Union
 from langchain.schema import AgentAction, AgentFinish
+from langchain.memory import ConversationBufferWindowMemory
 import re
 
-tools = [
+class CustomAgent:
+    def __init__(self, tools):
+        self.tools = tools
+        self.template_with_history = """Answer the following questions as best you can, but speaking as a pirate might speak. You have access to the following tools:
 
-]
+            {tools}
 
-# Set up the base template
-template = """Answer the following questions as best you can, but speaking as a pirate might speak. You have access to the following tools:
+            Use the following format:
 
-{tools}
+            Question: the input question you must answer
+            Thought: you should always think about what to do
+            Action: the action to take, should be one of [{tool_names}]
+            Action Input: the input to the action
+            Observation: the result of the action
+            ... (this Thought/Action/Action Input/Observation can repeat N times)
+            Thought: I now know the final answer
+            Final Answer: the final answer to the original input question
 
-Use the following format:
+            Begin! Remember to speak as a pirate when giving your final answer. Use lots of "Arg"s
 
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
+            Previous conversation history:
+            {history}
 
-Begin! Remember to speak as a pirate when giving your final answer. Use lots of "Arg"s
+            New question: {input}
+            {agent_scratchpad}"""
+        
+        self.prompt = self.CustomPromptTemplate(
+            template=self.template_with_history,
+            tools=self.tools,
+            input_variables=["input", "intermediate_steps"]
+        )
+        self.output_parser = self.CustomOutputParser()
 
-Question: {input}
-{agent_scratchpad}"""
+        self.llm = OpenAI(temperature=0)
 
-# Set up a prompt template
-class CustomPromptTemplate(StringPromptTemplate):
-    # The template to use
-    template: str
-    # The list of tools available
-    tools: List[Tool]
+        self.llm_chain = LLMChain(llm=self.llm, prompt=self.prompt_with_history)
+
+        self.tool_names = [tool.name for tool in self.tools]
+
+        self.agent = LLMSingleActionAgent(
+            llm_chain=self.llm_chain, 
+            output_parser=self.output_parser,
+            stop=["\nObservation:"], 
+            allowed_tools=self.tool_names
+        )
+
+        self.memory=ConversationBufferWindowMemory(k=2)
+
+        self.agent_executor = AgentExecutor.from_agent_and_tools(agent=self.agent, tools=self.tools, verbose=True, memory=self.memory)
+
+    class CustomPromptTemplate(StringPromptTemplate):
+        def format(self, **kwargs) -> str:
+            intermediate_steps = kwargs.pop("intermediate_steps")
+            thoughts = ""
+            for action, observation in intermediate_steps:
+                thoughts += action.log
+                thoughts += f"\nObservation: {observation}\nThought: "
+            kwargs["agent_scratchpad"] = thoughts
+            kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
+            kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
+            return self.template.format(**kwargs)
+
+    class CustomOutputParser(AgentOutputParser):
+        def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
+            if "Final Answer:" in llm_output:
+                return AgentFinish(
+                    return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
+                    log=llm_output,
+                )
+            regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
+            match = re.search(regex, llm_output, re.DOTALL)
+            if not match:
+                raise ValueError(f"Could not parse LLM output: `{llm_output}`")
+            action = match.group(1).strip()
+            action_input = match.group(2)
+            return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
     
-    def format(self, **kwargs) -> str:
-        # Get the intermediate steps (AgentAction, Observation tuples)
-        # Format them in a particular way
-        intermediate_steps = kwargs.pop("intermediate_steps")
-        thoughts = ""
-        for action, observation in intermediate_steps:
-            thoughts += action.log
-            thoughts += f"\nObservation: {observation}\nThought: "
-        # Set the agent_scratchpad variable to that value
-        kwargs["agent_scratchpad"] = thoughts
-        # Create a tools variable from the list of tools provided
-        kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
-        # Create a list of tool names for the tools provided
-        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
-        return self.template.format(**kwargs)
-    
-prompt = CustomPromptTemplate(
-    template=template,
-    tools=tools,
-    # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
-    # This includes the `intermediate_steps` variable because that is needed
-    input_variables=["input", "intermediate_steps"]
-)
+    # @TODO implement
+    def restart():
+        return 1
 
-class CustomOutputParser(AgentOutputParser):
-    
-    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
-        # Check if agent should finish
-        if "Final Answer:" in llm_output:
-            return AgentFinish(
-                # Return values is generally always a dictionary with a single `output` key
-                # It is not recommended to try anything else at the moment :)
-                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
-                log=llm_output,
-            )
-        # Parse out the action and action input
-        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
-        match = re.search(regex, llm_output, re.DOTALL)
-        if not match:
-            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
-        action = match.group(1).strip()
-        action_input = match.group(2)
-        # Return the action and action input
-        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
-    
-output_parser = CustomOutputParser()
+    def intake(self, input_str):
+       return self.agent_executor.run(input_str)
 
-llm = OpenAI(temperature=0)
-
-# LLM chain consisting of the LLM and a prompt
-llm_chain = LLMChain(llm=llm, prompt=prompt)
-
-tool_names = [tool.name for tool in tools]
-agent = LLMSingleActionAgent(
-    llm_chain=llm_chain, 
-    output_parser=output_parser,
-    stop=["\nObservation:"], 
-    allowed_tools=tool_names
-)
-
-agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
-
-agent_executor.run("How many people live in canada as of 2023?")
+# Usage:
+custom_agent = CustomAgent(tools=[])
+custom_agent.run("How many people live in canada as of 2023?")
